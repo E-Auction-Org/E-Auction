eaeb3017f03117353d66a2247c10208b71de3bf2
Juan Rojas <juan.rojas-castr@mail.escuelaing.edu.co>
2022-10-25 20:06:01 -0500

    Cors policy

--- src/main/java/edu/eci/eauction/config/AuctionWebSocketConfig.java
+++ src/main/java/edu/eci/eauction/config/AuctionWebSocketConfig.java
@@ -39,7 +39,7 @@ public class AuctionWebSocketConfig implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").withSockJS();
+        registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
     }
 
 }
